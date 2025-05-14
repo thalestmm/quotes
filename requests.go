e29b0b03c706914c433f83056904e301d0f92c12
thalestmm <meier.thales@gmail.com>
2025-05-13 23:58:24 -0300

    added more entries

--- requests.go
+++ requests.go
@@ -8,7 +8,10 @@ import (
 	"net/http"
 )
 
-func JSONFromEndpoint(c *fiber.Ctx, endpoint string) (map[string] interface, error) {
+func JSONFromEndpoint(c *fiber.Ctx, endpoint string) (
+	map[string]any,
+	error,
+) {
 	// Make a request to the JSON API
 	url := "http://localhost:3000/api/v1" + endpoint
 	resp, err := http.Get(url)
