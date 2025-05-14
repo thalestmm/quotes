a6786a7d24c445c37b4fd98c82e7e9c1eeb0f011
thalestmm <meier.thales@gmail.com>
2025-05-13 22:24:54 -0300

    added air

--- routes.go
+++ routes.go
@@ -6,4 +6,11 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+	v1.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Index")
+	})
 }
